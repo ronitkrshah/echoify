549bca07b226d9dde2fd135e4d6d2908247496b4
RKS <ronitkrshah@tuta.io>
2025-06-23 20:04:15 +0530

    perf: optimize recents data page load

--- src/features/recents/RecentScreen.tsx
+++ src/features/recents/RecentScreen.tsx
@@ -2,7 +2,7 @@ import { NativeBottomTabScreenProps } from "@bottom-tabs/react-navigation";
 import { CompositeScreenProps } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useEffect, useState } from "react";
-import { FlatList, View } from "react-native";
+import { FlatList, RefreshControl, View } from "react-native";
 import Animated, { FadeInDown } from "react-native-reanimated";
 import { SongEntity } from "~/database/entities";
 import { TStackNavigationRoutes } from "~/navigation";
@@ -10,7 +10,7 @@ import { TBottomTabRoutes } from "~/navigation/BottomTabNavigation";
 import RecentsRepository from "~/repositories/RecentsRepository";
 import { MusicListItem } from "../__shared__/components";
 import { Music } from "~/models";
-import { Text, useTheme } from "react-native-paper";
+import { ActivityIndicator, Text, useTheme } from "react-native-paper";
 import { useLoadingDialog } from "~/core/components";
 import { VirtualMusicPlayerService } from "~/core/services";
 import { asyncFuncExecutor } from "~/core/utils";
@@ -20,6 +20,8 @@ type TProps = CompositeScreenProps<
   NativeBottomTabScreenProps<TBottomTabRoutes, "RecentsScreen">,
   NativeStackScreenProps<TStackNavigationRoutes>
 >;
+
+const _perPageData = 20;
 
 export default function RecentScreen({ navigation }: TProps) {
   const [recentSongs, setRecentSongs] = useState<SongEntity[]>([]);
@@ -45,13 +47,20 @@ export default function RecentScreen({ navigation }: TProps) {
     }
   }
 
-  useEffect(() => {
-    const unsubscribe = navigation.addListener("focus", () => {
-      RecentsRepository.getAllMusicsAsync().then((data) => {
-        setRecentSongs(data ?? []);
-      });
-    });
+  async function getRecentsDataAsync(pageNumber = 0) {
+    const data = await RecentsRepository.getLimitedMusicsAsync(
+      pageNumber * _perPageData,
+      _perPageData
+    );
+    if (data) {
+      setRecentSongs((p) => [...p, ...data]);
+    }
+  }
 
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {});
+    setRecentSongs([]);
+    getRecentsDataAsync();
     return unsubscribe;
   }, []);
 
@@ -64,7 +73,9 @@ export default function RecentScreen({ navigation }: TProps) {
       </View>
       <FlatList
         data={recentSongs}
-        keyExtractor={(item) => item.id.toString()}
+        onEndReachedThreshold={0.1}
+        onEndReached={() => getRecentsDataAsync(recentSongs.length / _perPageData)}
+        keyExtractor={(item, index) => `${item.id}_${item.songId}_${index}`}
         contentContainerStyle={{ paddingHorizontal: 8, gap: 16 }}
         renderItem={({ item, index }) => (
           <Animated.View
